b9988b41fd65ac15deea22795c70a9042334fb6b
Jasper <54336703+JazJax@users.noreply.github.com>
2024-09-18 17:23:06 +0100

    It deploys!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (#6)
    * Uncommented build checks, and deleted superfluous file that made it in
    
    * Added deploy-dev to the list of branches which are deployed to dev; explicitly filtered the preprod check to main
    
    * Updated readme with deployment and logs info
    
    * edited circleci config - erroneously added a branch to filter (but in commented code, so no biggie)
    
    * re-removed filter on validate task
    
    * Now working??!

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/health/HmppsAuthHealthPing.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/health/HmppsAuthHealthPing.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.templatepackagename.health
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.health
 
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
@@ -6,10 +6,6 @@ import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.hmpps.kotlin.health.HealthPingCheck
 
 // HMPPS Auth health ping is required if your service calls HMPPS Auth to get a token to call other services
-// Remove the health ping if no call outs to other services are made
+// TODO: Remove the health ping if no call outs to other services are made
 @Component("hmppsAuth")
 class HmppsAuthHealthPing(@Qualifier("hmppsAuthHealthWebClient") webClient: WebClient) : HealthPingCheck(webClient)
-
-// Example ping health check calling out to other services
-// @Component("exampleApi")
-// class ExampleApiHealthPing(@Qualifier("exampleApiHealthWebClient") webClient: WebClient) : HealthPingCheck(webClient)
