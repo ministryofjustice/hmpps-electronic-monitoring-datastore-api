880f4aa111973db3851eae06e5442569456ebaed
jasper.jackson <jasper.jackson@madetech.com>
2024-10-09 11:12:16 +0100

    Require parameter for case search

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/controllers/SearchController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/controllers/SearchController.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.controller
 
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,6 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = ["/search"], produces = ["application/json"])
 class SearchController {
 
-  @GetMapping("/cases")
-  fun getCases(): String = "true"
+  @GetMapping("/cases/{caseID}")
+  fun getCases(
+    @PathVariable(
+      required = true,
+      name = "caseID",
+    ) caseId: String,
+  ): String {
+    val response = "Successful search with ID $caseId"
+    return response
+  }
 }
