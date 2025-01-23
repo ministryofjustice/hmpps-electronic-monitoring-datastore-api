835067e8ed88a29386f16f3f145c08d3a844a1e0
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-01-23 17:00:39 +0000

    Updated dummy athena response in OrderDetailsRepositoryTest - tests now pass

--- src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/OrderDetailsRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/OrderDetailsRepositoryTest.kt
@@ -2,9 +2,15 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.repository
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.kotlin.any
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.EmDatastoreClient
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.AthenaHelper
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaOrderDetailsDTO
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaQuery
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.testutils.metaDataRow
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.testutils.varCharValueColumn
 import java.util.*
@@ -13,15 +19,21 @@ class OrderDetailsRepositoryTest {
   private lateinit var emDatastoreClient: EmDatastoreClient
   private lateinit var repository: OrderDetailsRepository
 
-  fun orderDetailsData(orderId: String) = """
+  fun orderDetailsData(subjectId: String, orderId: String, offenceRisk: Boolean) = """
     {
       "Data": [
+        ${varCharValueColumn(subjectId)},
         ${varCharValueColumn(orderId)},
+        ${varCharValueColumn(offenceRisk.toString())},
       ]
     }
   """.trimIndent()
 
-  fun orderDetailsResultSet(orderId: String? = "1234") = """
+  fun orderDetailsResultSet(
+    subjectId: String? = "1234",
+    orderId: String? = "fakeOrderId",
+    offenceRisk: Boolean = true,
+  ) = """
     {
       "ResultSet": {
         "Rows": [
@@ -30,12 +42,14 @@ class OrderDetailsRepositoryTest {
               ${varCharValueColumn("legacy_subject_id")},
             ]
           },
-          ${orderDetailsData(orderId!!)},
-          ${orderDetailsData("5678")}
+          ${orderDetailsData(subjectId!!, orderId!!, offenceRisk)},
+          ${orderDetailsData("5678", "This row should never be returned", false)}
         ],
         "ResultSetMetadata": {
           "ColumnInfo": [
-            """ + metaDataRow("legacy_subject_id") + """,
+            ${metaDataRow("legacy_subject_id")},
+            ${metaDataRow("legacy_order_id")},
+            ${metaDataRow("offence_risk", "boolean")}
           ]
         }
       },
@@ -55,41 +69,41 @@ class OrderDetailsRepositoryTest {
     Assertions.assertThat(sut).isNotNull()
   }
 
-//  @Nested
-//  inner class GetOrderDetails {
-//    @Test
-//    fun `getOrderDetails calls getQueryResult`() {
-//      val resultSet = AthenaHelper.resultSetFromJson(orderDetailsResultSet())
-//
-//      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
-//
-//      repository.getOrderDetails("123", AthenaRole.DEV)
-//
-//      Mockito.verify(emDatastoreClient).getQueryResult(any<AthenaQuery>(), any<AthenaRole>())
-//    }
-//
-//    @Test
-//    fun `getOrderDetails returns an AthenaOrderDetailsDTO`() {
-//      val resultSet = AthenaHelper.resultSetFromJson(orderDetailsResultSet())
-//
-//      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
-//
-//      val result = repository.getOrderDetails("123", AthenaRole.DEV)
-//
-//      Assertions.assertThat(result).isInstanceOf(AthenaOrderDetailsDTO::class.java)
-//    }
-//
-//    @Test
-//    fun `getOrderDetails returns the first result from getQueryResult`() {
-//      val orderId = "expectedId"
-//      val resultSet = AthenaHelper.resultSetFromJson(orderDetailsResultSet(orderId))
-//
-//      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
-//
-//      val result = repository.getOrderDetails(orderId, AthenaRole.DEV)
-//
-//      Assertions.assertThat(result).isNotNull
-//      Assertions.assertThat(result.legacySubjectId).isEqualTo(orderId)
-//    }
-//  }
+  @Nested
+  inner class GetOrderDetails {
+    @Test
+    fun `getOrderDetails calls getQueryResult`() {
+      val resultSet = AthenaHelper.resultSetFromJson(orderDetailsResultSet())
+
+      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
+
+      repository.getOrderDetails("123", AthenaRole.DEV)
+
+      Mockito.verify(emDatastoreClient).getQueryResult(any<AthenaQuery>(), any<AthenaRole>())
+    }
+
+    @Test
+    fun `getOrderDetails returns an AthenaOrderDetailsDTO`() {
+      val resultSet = AthenaHelper.resultSetFromJson(orderDetailsResultSet())
+
+      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
+
+      val result = repository.getOrderDetails("123", AthenaRole.DEV)
+
+      Assertions.assertThat(result).isInstanceOf(AthenaOrderDetailsDTO::class.java)
+    }
+
+    @Test
+    fun `getOrderDetails returns the first result from getQueryResult`() {
+      val orderId = "expectedId"
+      val resultSet = AthenaHelper.resultSetFromJson(orderDetailsResultSet(orderId))
+
+      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
+
+      val result = repository.getOrderDetails(orderId, AthenaRole.DEV)
+
+      Assertions.assertThat(result).isNotNull
+      Assertions.assertThat(result.legacySubjectId).isEqualTo(orderId)
+    }
+  }
 }
