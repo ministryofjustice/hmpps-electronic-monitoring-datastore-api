72da51b13b2fbcccdfa3ef305df674365dfdff0a
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-01-23 16:01:12 +0000

    Added order details service with tests.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/service/OrderService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/service/OrderService.kt
@@ -5,10 +5,15 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.Document
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.KeyOrderInformation
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.OrderDetails
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.OrderInformation
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.OrderSearchCriteria
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.OrderSearchResult
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.SubjectHistoryReport
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaDocumentListDTO
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaKeyOrderInformationDTO
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaOrderDetailsDTO
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaOrderSearchResultDTO
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaStringQuery
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.repository.OrderInformationRepository
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.repository.OrderRepository
@@ -60,5 +65,62 @@ class OrderService(
       subjectHistoryReport = emptyHistoryReport,
       documents = listOf<Document>(),
     )
+  }
+
+  fun getOrderDetails(orderId: String, role: AthenaRole): OrderDetails {
+    val orderDetails: AthenaOrderDetailsDTO = AthenaOrderDetailsDTO("")
+//    val orderDetails: AthenaOrderDetailsDTO = orderDetailsRepository.getOrderDetails(orderId, role)
+    val parsedOrderDetails = parseOrderDetails(orderDetails)
+
+    return parsedOrderDetails
+  }
+
+  private fun parseOrderSearchResults(athenaOrderSearchResultList: List<AthenaOrderSearchResultDTO>): List<OrderSearchResult> {
+    var orderSearchResults = athenaOrderSearchResultList.map { athenaOrderSearchResult -> OrderSearchResult(athenaOrderSearchResult) }
+
+    // TODO: The field list being returned doesn't match 'order' object - this needs resolving asap!
+    // Solution: use an OrderDTO object? I suspect this is the best approach.Or just map to an Order object that the UI needs
+    // Probably: rename Order to OrderDTO and have an internal Order class that matches the SQL
+    return orderSearchResults
+  }
+
+  private fun parseKeyOrderInformation(athenaKeyOrderInformation: AthenaKeyOrderInformationDTO): KeyOrderInformation {
+    var keyOrderInformation = KeyOrderInformation(athenaKeyOrderInformation)
+
+    return keyOrderInformation
+  }
+
+  private fun parseOrderDetails(athenaOrderDetails: AthenaOrderDetailsDTO): OrderDetails {
+    val orderDetails = OrderDetails(athenaOrderDetails)
+
+    return orderDetails
+  }
+
+  private fun parseSubjectHistoryReport(athenaSubjectHistoryReport: AthenaSubjectHistoryReportDTO): SubjectHistoryReport {
+    var subjectHistoryReport = SubjectHistoryReport(
+      name = athenaSubjectHistoryReport.name,
+      reportUrl = athenaSubjectHistoryReport.reportUrl,
+      createdOn = athenaSubjectHistoryReport.createdOn,
+      time = athenaSubjectHistoryReport.time,
+    )
+
+    return subjectHistoryReport
+  }
+
+  private fun parseDocumentList(athenaDocumentList: AthenaDocumentListDTO): DocumentList {
+    var documentList = DocumentList(
+      pageSize = athenaDocumentList.pageSize,
+      orderDocuments = athenaDocumentList.orderDocuments.map { document ->
+        Document(
+          name = document.name,
+          url = document.url,
+          notes = document.notes,
+          createdOn = document.createdOn,
+          time = document.time,
+        )
+      },
+    )
+
+    return documentList
   }
 }
