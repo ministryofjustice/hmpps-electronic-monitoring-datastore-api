2e139cab38d0ff6b19af7dc302a5222f78c77036
Jasper <54336703+JazJax@users.noreply.github.com>
2025-01-23 16:01:12 +0000

    added endpoint, no service yet

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/model/athena/AthenaOrderDetailsDTO.kt
@@ -0,0 +1,7 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena
+
+data class AthenaOrderDetailsDTO(
+  val legacySubjectId: String,
+)
+
+// TODO: Get fields from miro
