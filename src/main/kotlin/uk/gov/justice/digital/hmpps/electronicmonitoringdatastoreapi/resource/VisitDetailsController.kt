c687a075eb9ba933e73723e2d1a886cac47ffbb0
mattonfoot <matt.smith@justice.gov.uk>
2025-02-07 09:18:48 +0000

    Add Visit Details resource

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/VisitDetailsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/VisitDetailsController.kt
@@ -9,37 +9,36 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.SuspensionOfVisits
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.VisitDetails
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.AthenaRoleService
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.SuspensionOfVisitsService
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.VisitDetailsService
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.internal.AuditService
 
 @RestController
 @PreAuthorize("hasAnyAuthority('ROLE_EM_DATASTORE_GENERAL_RO', 'ROLE_EM_DATASTORE_RESTRICTED_RO')")
 @RequestMapping(value = ["/orders"], produces = [MediaType.APPLICATION_JSON_VALUE])
-class SuspensionOfVisitsController(
-  @Autowired val suspensionOfVisitsService: SuspensionOfVisitsService,
+class VisitDetailsController(
+  @Autowired val visitDetailsService: VisitDetailsService,
   val athenaRoleService: AthenaRoleService,
 
   // TODO: Re-enable audit as @autowired once Cloud Platform in place
   val auditService: AuditService? = null,
 ) {
-  @GetMapping("/getSuspensionOfVisits/{orderId}")
-  fun getSuspensionOfVisits(
+  @GetMapping("/getVisitDetails/{orderId}")
+  fun getVisitDetails(
     authentication: Authentication,
     @PathVariable(required = true) orderId: String,
-  ): ResponseEntity<List<SuspensionOfVisits>> {
+  ): ResponseEntity<List<VisitDetails>> {
     val validatedRole = athenaRoleService.getRoleFromAuthentication(authentication)
 
-    val result = suspensionOfVisitsService.getSuspensionOfVisits(orderId, validatedRole)
+    val result = visitDetailsService.getVisitDetails(orderId, validatedRole)
 
     auditService?.createEvent(
       authentication.name,
-      "GET_SUSPENSION_OF_VISITS",
+      "GET_VISIT_DETAILS",
       mapOf("orderId" to orderId),
     )
 
     return ResponseEntity.ok(result)
   }
 }
-
