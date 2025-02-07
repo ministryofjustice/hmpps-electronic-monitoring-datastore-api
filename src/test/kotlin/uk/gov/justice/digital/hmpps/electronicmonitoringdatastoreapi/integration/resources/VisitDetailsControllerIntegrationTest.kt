4d5dea3b0d4531df95c5e3f87cddb119c6fe1dcc
mattonfoot <matt.smith@justice.gov.uk>
2025-02-07 09:34:49 +0000

    Add Visit Details integration testing and mocks

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/integration/resources/VisitDetailsControllerIntegrationTest.kt
@@ -0,0 +1,47 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.integration.resources
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.mocks.MockEmDatastoreClient
+
+@ActiveProfiles("integration")
+class VisitDetailsControllerIntegrationTest : ControllerIntegrationBase() {
+  @Nested
+  @DisplayName("GET /orders/getVisitDetails/{orderId}")
+  inner class GetVisitDetails {
+    val baseUri = "/orders/getVisitDetails"
+
+    @BeforeEach
+    fun setup() {
+      MockEmDatastoreClient.Companion.addResponseFile("successfulVisitDetailsResponse")
+    }
+
+    @Test
+    fun `should return 401 unauthorized if no authorization header`() {
+      noAuthHeaderRespondsWithUnauthorizedTest("$baseUri/234")
+    }
+
+    @Test
+    fun `should return 403 forbidden if no role in authorization header`() {
+      noRoleInAuthHeaderRespondsWithForbiddenTest("$baseUri/234")
+    }
+
+    @Test
+    fun `should return 403 forbidden if wrong role in authorization header`() {
+      wrongRolesRespondsWithForbiddenTest("$baseUri/234", listOf("ROLE_WRONG"))
+    }
+
+    @Test
+    fun `should return OK with valid auth header, role`() {
+      webTestClient.get()
+        .uri("$baseUri/234")
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus()
+        .isOk
+    }
+  }
+}
