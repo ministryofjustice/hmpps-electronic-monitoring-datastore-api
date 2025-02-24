f8640ebc208e3785a22139040f96047fc2b52611
mattonfoot <matt.smith@justice.gov.uk>
2025-02-24 10:14:58 +0000

    Add support for handling verification of passed MFA claim

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/config/AuthAwareTokenConverter.kt
@@ -0,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config
+
+import org.springframework.core.convert.converter.Converter
+import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
+
+class AuthAwareTokenConverter : Converter<Jwt, AbstractAuthenticationToken> {
+  private val jwtGrantedAuthoritiesConverter: Converter<Jwt, Collection<GrantedAuthority>> =
+    JwtGrantedAuthoritiesConverter()
+
+  override fun convert(jwt: Jwt): AbstractAuthenticationToken {
+    verifyPassedMFA(jwt)
+
+    val principal = extractPrincipal(jwt)
+    val authorities = extractAuthorities(jwt)
+
+    return AuthAwareAuthenticationToken(jwt, principal, authorities)
+  }
+
+  @Throws(AuthenticationException::class)
+  private fun verifyPassedMFA(jwt: Jwt) {
+    val passedMFA = jwt.claims["passed_MFA"]
+
+    if (passedMFA != "true") {
+      throw InvalidBearerTokenException("Multi-factor authentication must have been used as part of your authentication")
+    }
+  }
+
+  @Throws(AuthenticationException::class)
+  private fun extractPrincipal(jwt: Jwt): String {
+    val subject = jwt.subject
+
+    if (subject == null) {
+      throw InvalidBearerTokenException("Username is not in token")
+    }
+
+    return subject
+  }
+
+  private fun extractAuthorities(jwt: Jwt): Collection<GrantedAuthority> {
+    val authorities = mutableListOf<GrantedAuthority>().apply {
+      addAll(jwtGrantedAuthoritiesConverter.convert(jwt)!!)
+    }
+
+    if (jwt.claims.containsKey(CLAIM_AUTHORITY)) {
+      @Suppress("UNCHECKED_CAST")
+      val claimAuthorities = (jwt.claims[CLAIM_AUTHORITY] as Collection<String>).toList()
+      authorities.addAll(claimAuthorities.map(::SimpleGrantedAuthority))
+    }
+
+    if (authorities.isEmpty()) {
+      throw InvalidBearerTokenException("No roles in token")
+    }
+
+    return authorities.toSet()
+  }
+
+  companion object {
+    const val CLAIM_AUTHORITY = "authorities"
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/config/OAuth2ResourceServerSecurityConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/config/OAuth2ResourceServerSecurityConfiguration.kt
@@ -2,9 +2,7 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.convert.converter.Converter
 import org.springframework.http.HttpStatus
-import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -12,13 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.AuthenticationException
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 
@@ -60,68 +51,4 @@ class OAuth2ResourceServerSecurityConfiguration {
 
     return http.build()
   }
-}
-
-class AuthAwareTokenConverter : Converter<Jwt, AbstractAuthenticationToken> {
-  private val jwtGrantedAuthoritiesConverter: Converter<Jwt, Collection<GrantedAuthority>> =
-    JwtGrantedAuthoritiesConverter()
-
-  override fun convert(jwt: Jwt): AbstractAuthenticationToken {
-    validatePassedMFA(jwt)
-
-    val principal = extractPrincipal(jwt)
-    val authorities = extractAuthorities(jwt)
-
-    return AuthAwareAuthenticationToken(jwt, principal, authorities)
-  }
-
-  @Throws(AuthenticationException::class)
-  private fun extractPrincipal(jwt: Jwt): String {
-    val subject = jwt.subject
-
-    if (subject == null) {
-      throw InvalidBearerTokenException("Username is not in token")
-    }
-
-    return subject
-  }
-
-  private fun extractAuthorities(jwt: Jwt): Collection<GrantedAuthority> {
-    val authorities = mutableListOf<GrantedAuthority>().apply {
-      addAll(jwtGrantedAuthoritiesConverter.convert(jwt)!!)
-    }
-
-    if (jwt.claims.containsKey(CLAIM_AUTHORITY)) {
-      @Suppress("UNCHECKED_CAST")
-      val claimAuthorities = (jwt.claims[CLAIM_AUTHORITY] as Collection<String>).toList()
-      authorities.addAll(claimAuthorities.map(::SimpleGrantedAuthority))
-    }
-
-    if (authorities.isEmpty()) {
-      throw InvalidBearerTokenException("No roles in token")
-    }
-
-    return authorities.toSet()
-  }
-
-  @Throws(AuthenticationException::class)
-  private fun validatePassedMFA(jwt: Jwt) {
-    val passedMFA = jwt.claims["passed_mfa"] as Boolean?
-
-    if (passedMFA != true) {
-      throw InvalidBearerTokenException("MFA was '$passedMFA' which is not true")
-    }
-  }
-
-  companion object {
-    const val CLAIM_AUTHORITY = "authorities"
-  }
-}
-
-class AuthAwareAuthenticationToken(
-  jwt: Jwt,
-  private val aPrincipal: String,
-  authorities: Collection<GrantedAuthority>,
-) : JwtAuthenticationToken(jwt, authorities, aPrincipal) {
-  override fun getPrincipal(): String = aPrincipal
 }
