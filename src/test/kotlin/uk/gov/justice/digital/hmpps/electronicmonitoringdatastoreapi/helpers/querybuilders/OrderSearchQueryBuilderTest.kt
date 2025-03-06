7a08ecc03972752c3384487ff0e7f6c497ea9362
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-03-06 12:58:57 +0000

    Update tests.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/OrderSearchQueryBuilderTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/OrderSearchQueryBuilderTest.kt
@@ -10,10 +10,12 @@ class OrderSearchQueryBuilderTest {
         SELECT 
           legacy_subject_id
           , full_name
+          , alias
           , primary_address_line_1
           , primary_address_line_2
           , primary_address_line_3
           , primary_address_post_code
+          , date_of_birth
           , order_start_date
           , order_end_date 
         FROM 
@@ -130,7 +132,7 @@ class OrderSearchQueryBuilderTest {
     val expectedSQL = replaceWhitespace(
       baseQuery + """
             legacy_subject_id=$legacySubjectId
-            OR alias LIKE UPPER('%$alias%')
+            AND alias LIKE UPPER('%$alias%')
       """.trimIndent(),
     )
 
