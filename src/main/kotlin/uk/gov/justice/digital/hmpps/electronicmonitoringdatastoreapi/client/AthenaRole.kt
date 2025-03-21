8f397778ee10915b816fd3fd0ab8799e40a9e4c2
mattonfoot <matt.smith@justice.gov.uk>
2025-03-21 20:59:05 +0000

    Merge AthenaRole assignment logic into EMDatastoreClient

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/client/AthenaRole.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/client/AthenaRole.kt
@@ -1,15 +1,6 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client
 
-enum class AthenaRole(
-  val priority: Int,
-) {
-  ROLE_EM_DATASTORE_RESTRICTED_RO(
-    priority = 20,
-  ),
-  ROLE_EM_DATASTORE_GENERAL_RO(
-    priority = 10,
-  ),
-  NONE(
-    priority = 0,
-  ),
+enum class AthenaRole {
+  ROLE_EM_DATASTORE_RESTRICTED_RO,
+  ROLE_EM_DATASTORE_GENERAL_RO,
 }
