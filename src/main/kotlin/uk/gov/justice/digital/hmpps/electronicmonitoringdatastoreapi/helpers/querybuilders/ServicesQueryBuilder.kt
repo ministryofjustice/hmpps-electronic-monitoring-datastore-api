e072665de41b34f26ca58d380ea0f489bc2b8a25
mattonfoot <matt.smith@justice.gov.uk>
2025-03-22 11:39:09 +0000

    Use Zeko for Services

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/ServicesQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/ServicesQueryBuilder.kt
@@ -3,42 +3,37 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.qu
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaServicesQuery
 
 class ServicesQueryBuilder(
-  var databaseName: String? = null,
+  override var databaseName: String,
+) : SqlQueryBuilder(
+  databaseName,
+  "services",
+  arrayOf(
+    "legacy_subject_id",
+    "legacy_order_id",
+    "service_id",
+    "service_address_1",
+    "service_address_2",
+    "service_address_3",
+    "service_address_postcode",
+    "service_start_date",
+    "service_end_date",
+    "curfew_start_date",
+    "curfew_end_date",
+    "curfew_start_time",
+    "curfew_end_time",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ),
 ) {
-  var legacySubjectId: String? = null
-
   fun withLegacySubjectId(subjectId: String): ServicesQueryBuilder {
-    legacySubjectId = subjectId
+    parameters["legacy_subject_id"] = subjectId
     return this
   }
 
-  fun build(): AthenaServicesQuery = AthenaServicesQuery(
-    """
-      SELECT
-          legacy_subject_id
-        , service_id
-        , service_address_1
-        , service_address_2
-        , service_address_3
-        , service_address_postcode
-        , service_start_date
-        , service_end_date
-        , curfew_start_date
-        , curfew_end_date
-        , curfew_start_time
-        , curfew_end_time
-        , monday
-        , tuesday
-        , wednesday
-        , thursday
-        , friday
-        , saturday
-        , sunday
-      FROM
-        $databaseName.services
-      WHERE
-        legacy_subject_id = $legacySubjectId
-    """.trimIndent(),
-    arrayOf(),
-  )
+  fun build(): AthenaServicesQuery = AthenaServicesQuery(getSQL(), parameters.values.toTypedArray())
 }
