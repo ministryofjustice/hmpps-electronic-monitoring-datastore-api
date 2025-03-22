92c6fbb0e9cef7721e85af042c11b4e0b35b668f
mattonfoot <matt.smith@justice.gov.uk>
2025-03-22 11:29:30 +0000

    Use Zeko for SuspensionOfVisits

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/SuspensionOfVisitsQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/SuspensionOfVisitsQueryBuilder.kt
@@ -3,29 +3,24 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.qu
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaSuspensionOfVisitsListQuery
 
 class SuspensionOfVisitsQueryBuilder(
-  var databaseName: String? = null,
+  override var databaseName: String,
+) : SqlQueryBuilder(
+  databaseName,
+  "suspension_of_visits",
+  arrayOf(
+    "legacy_subject_id",
+    "legacy_order_id",
+    "suspension_of_visits",
+    "suspension_of_visits_requested_date",
+    "suspension_of_visits_start_date",
+    "suspension_of_visits_start_time",
+    "suspension_of_visits_end_date",
+  ),
 ) {
-  var legacySubjectId: String? = null
-
   fun withLegacySubjectId(subjectId: String): SuspensionOfVisitsQueryBuilder {
-    legacySubjectId = subjectId
+    parameters["legacy_subject_id"] = subjectId
     return this
   }
 
-  fun build(): AthenaSuspensionOfVisitsListQuery = AthenaSuspensionOfVisitsListQuery(
-    """
-      SELECT
-          legacy_subject_id
-        , suspension_of_visits
-        , suspension_of_visits_requested_date
-        , suspension_of_visits_start_date
-        , suspension_of_visits_start_time
-        , suspension_of_visits_end_date
-      FROM
-        $databaseName.suspension_of_visits
-      WHERE
-        legacy_subject_id = $legacySubjectId
-    """.trimIndent(),
-    arrayOf(),
-  )
+  fun build(): AthenaSuspensionOfVisitsListQuery = AthenaSuspensionOfVisitsListQuery(getSQL(), parameters.values.toTypedArray())
 }
