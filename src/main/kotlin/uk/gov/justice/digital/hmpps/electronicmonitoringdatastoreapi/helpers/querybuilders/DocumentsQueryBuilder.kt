559b6237f37e9f1368f6f5438bb7c0366e726761
mattonfoot <matt.smith@justice.gov.uk>
2025-03-24 10:16:44 +0000

    Use Zeko for Documents

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/DocumentsQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/DocumentsQueryBuilder.kt
@@ -1,22 +1,26 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.querybuilders
 
+import org.apache.commons.lang3.StringUtils.isAlphanumeric
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaDocumentListQuery
 
 class DocumentsQueryBuilder(
-  var databaseName: String? = null,
+  override val databaseName: String,
+) : SqlQueryBuilder(
+  databaseName,
+  "equipment_details",
+  arrayOf(
+    "legacy_subject_id",
+    "legacy_order_id",
+  ),
 ) {
-  var legacySubjectId: String? = null
-
   fun withLegacySubjectId(subjectId: String): DocumentsQueryBuilder {
-    legacySubjectId = subjectId
+    if (!isAlphanumeric(subjectId)) {
+      throw IllegalArgumentException("Input contains illegal characters")
+    }
+
+    parameters["legacy_subject_id"] = subjectId
     return this
   }
 
-  fun build(): AthenaDocumentListQuery = AthenaDocumentListQuery(
-    """
-      SELECT
-         $legacySubjectId as legacy_order_id
-    """.trimIndent(),
-    arrayOf(),
-  )
+  fun build(): AthenaDocumentListQuery = AthenaDocumentListQuery(getSQL(), parameters.values.toTypedArray())
 }
