5440434648df5998d1c74ac6ede444eb333bea8e
mattonfoot <matt.smith@justice.gov.uk>
2025-03-24 10:14:41 +0000

    Use Zeko for Equipment Details

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/EquipmentDetailsQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/EquipmentDetailsQueryBuilder.kt
@@ -1,39 +1,38 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.querybuilders
 
+import org.apache.commons.lang3.StringUtils.isAlphanumeric
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaEquipmentDetailsListQuery
 
 class EquipmentDetailsQueryBuilder(
-  var databaseName: String? = null,
+  override val databaseName: String,
+) : SqlQueryBuilder(
+  databaseName,
+  "equipment_details",
+  arrayOf(
+    "legacy_subject_id",
+    "legacy_order_id",
+    "hmu_id",
+    "hmu_equipment_category_description",
+    "hmu_install_date",
+    "hmu_install_time",
+    "hmu_removed_date",
+    "hmu_removed_time",
+    "pid_id",
+    "pid_equipment_category_description",
+    "date_device_installed",
+    "time_device_installed",
+    "date_device_removed",
+    "time_device_removed",
+  ),
 ) {
-  var legacySubjectId: String? = null
-
   fun withLegacySubjectId(subjectId: String): EquipmentDetailsQueryBuilder {
-    legacySubjectId = subjectId
+    if (!isAlphanumeric(subjectId)) {
+      throw IllegalArgumentException("Input contains illegal characters")
+    }
+
+    parameters["legacy_subject_id"] = subjectId
     return this
   }
 
-  fun build(): AthenaEquipmentDetailsListQuery = AthenaEquipmentDetailsListQuery(
-    """
-      SELECT
-        legacy_subject_id
-        , legacy_order_id
-        , hmu_id
-        , hmu_equipment_category_description
-        , hmu_install_date
-        , hmu_install_time
-        , hmu_removed_date
-        , hmu_removed_time
-        , pid_id
-        , pid_equipment_category_description
-        , date_device_installed
-        , time_device_installed
-        , date_device_removed
-        , time_device_removed
-      FROM
-        $databaseName.equipment_details
-      WHERE
-        legacy_subject_id = $legacySubjectId
-    """.trimIndent(),
-    arrayOf(),
-  )
+  fun build(): AthenaEquipmentDetailsListQuery = AthenaEquipmentDetailsListQuery(getSQL(), parameters.values.toTypedArray())
 }
