264156a28a9047a153119426bd5568df635e0b09
mattonfoot <matt.smith@justice.gov.uk>
2025-03-24 09:47:31 +0000

    Use Zeko for Key Order Information

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/KeyOrderInformationQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/KeyOrderInformationQueryBuilder.kt
@@ -1,36 +1,35 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.querybuilders
 
+import org.apache.commons.lang3.StringUtils.isAlphanumeric
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaKeyOrderInformationQuery
 
 class KeyOrderInformationQueryBuilder(
-  var databaseName: String? = null,
+  override val databaseName: String,
+) : SqlQueryBuilder(
+  databaseName,
+  "order_details",
+  arrayOf(
+    "legacy_subject_id",
+    "legacy_order_id",
+    "full_name",
+    "alias",
+    "date_of_birth",
+    "primary_address_line_1",
+    "primary_address_line_2",
+    "primary_address_line_3",
+    "primary_address_post_code",
+    "order_start_date",
+    "order_end_date",
+  ),
 ) {
-  var legacySubjectId: String? = null
-
   fun withLegacySubjectId(subjectId: String): KeyOrderInformationQueryBuilder {
-    legacySubjectId = subjectId
+    if (!isAlphanumeric(subjectId)) {
+      throw IllegalArgumentException("Input contains illegal characters")
+    }
+
+    parameters["legacy_subject_id"] = subjectId
     return this
   }
 
-  fun build(): AthenaKeyOrderInformationQuery = AthenaKeyOrderInformationQuery(
-    """
-      SELECT
-            legacy_subject_id
-          , legacy_order_id
-          , full_name
-          , alias
-          , date_of_birth
-          , primary_address_line_1
-          , primary_address_line_2
-          , primary_address_line_3
-          , primary_address_post_code
-          , order_start_date
-          , order_end_date
-      FROM 
-        $databaseName.order_details
-      WHERE
-        legacy_subject_id = $legacySubjectId
-    """.trimIndent(),
-    arrayOf(),
-  )
+  fun build(): AthenaKeyOrderInformationQuery = AthenaKeyOrderInformationQuery(getSQL(), parameters.values.toTypedArray())
 }
