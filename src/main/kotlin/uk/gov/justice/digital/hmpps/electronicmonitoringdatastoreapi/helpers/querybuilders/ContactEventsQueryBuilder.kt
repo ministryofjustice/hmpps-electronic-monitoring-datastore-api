0c9cf133e28b7b9d8e567622138652aaf59ad11d
mattonfoot <matt.smith@justice.gov.uk>
2025-03-24 10:19:52 +0000

    Use Zeko for Contact Events

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/ContactEventsQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/ContactEventsQueryBuilder.kt
@@ -1,37 +1,36 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.querybuilders
 
+import org.apache.commons.lang3.StringUtils.isAlphanumeric
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaContactEventsListQuery
 
 class ContactEventsQueryBuilder(
-  var databaseName: String? = null,
+  override val databaseName: String,
+) : SqlQueryBuilder(
+  databaseName,
+  "contact_history",
+  arrayOf(
+    "legacy_subject_id",
+    "legacy_order_id",
+    "outcome",
+    "contact_type",
+    "reason",
+    "channel",
+    "user_id",
+    "user_name",
+    "contact_date",
+    "contact_time",
+    "modified_date",
+    "modified_time",
+  ),
 ) {
-  var legacySubjectId: String? = null
-
   fun withLegacySubjectId(subjectId: String): ContactEventsQueryBuilder {
-    legacySubjectId = subjectId
+    if (!isAlphanumeric(subjectId)) {
+      throw IllegalArgumentException("Input contains illegal characters")
+    }
+
+    parameters["legacy_subject_id"] = subjectId
     return this
   }
 
-  fun build(): AthenaContactEventsListQuery = AthenaContactEventsListQuery(
-    """
-      SELECT
-        legacy_subject_id
-        , legacy_order_id
-        , outcome
-        , contact_type
-        , reason
-        , channel
-        , user_id
-        , user_name
-        , contact_date
-        , contact_time
-        , modified_date
-        , modified_time
-      FROM
-        $databaseName.contact_history
-      WHERE
-        legacy_subject_id = $legacySubjectId
-    """.trimIndent(),
-    arrayOf(),
-  )
+  fun build(): AthenaContactEventsListQuery = AthenaContactEventsListQuery(getSQL(), parameters.values.toTypedArray())
 }
