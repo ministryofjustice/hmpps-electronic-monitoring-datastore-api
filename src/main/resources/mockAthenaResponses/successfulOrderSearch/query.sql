137641249bba78e9fc933763646927169ac346be
mattonfoot <matt.smith@justice.gov.uk>
2025-03-24 12:56:29 +0000

    Update Mock client queries so they match the generated SQL

--- src/main/resources/mockAthenaResponses/successfulOrderSearch/query.sql
+++ src/main/resources/mockAthenaResponses/successfulOrderSearch/query.sql
@@ -1,13 +1,14 @@
 SELECT
-  legacy_subject_id
-  , full_name
-  , primary_address_line_1
-  , primary_address_line_2
-  , primary_address_line_3
-  , primary_address_post_code
-  , order_start_date
-  , order_end_date
+    legacy_subject_id
+     , full_name
+     , alias
+     , primary_address_line_1
+     , primary_address_line_2
+     , primary_address_line_3
+     , primary_address_post_code
+     , date_of_birth
+     , order_start_date
+     , order_end_date
 FROM
     historic_api_mart_integration.order_details
-WHERE
-    first_name LIKE UPPER('%any%')
+WHERE first_name LIKE UPPER('%any%')
