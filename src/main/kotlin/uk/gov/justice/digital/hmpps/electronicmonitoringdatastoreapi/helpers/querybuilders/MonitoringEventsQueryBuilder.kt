f55f15810429c16047a3db77cc6ea8611076a96a
mattonfoot <matt.smith@justice.gov.uk>
2025-03-24 09:35:12 +0000

    Use Zeko for Monitoring Events

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/MonitoringEventsQueryBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/querybuilders/MonitoringEventsQueryBuilder.kt
@@ -1,34 +1,33 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.querybuilders
 
+import org.apache.commons.lang3.StringUtils.isAlphanumeric
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.AthenaMonitoringEventsListQuery
 
 class MonitoringEventsQueryBuilder(
-  var databaseName: String? = null,
+  override val databaseName: String,
+) : SqlQueryBuilder(
+  databaseName,
+  "event_history",
+  arrayOf(
+    "legacy_subject_id",
+    "legacy_order_id",
+    "event_type",
+    "event_date",
+    "event_time",
+    "event_second",
+    "process_date",
+    "process_time",
+    "process_second",
+  ),
 ) {
-  var legacySubjectId: String? = null
-
   fun withLegacySubjectId(subjectId: String): MonitoringEventsQueryBuilder {
-    legacySubjectId = subjectId
+    if (!isAlphanumeric(subjectId)) {
+      throw IllegalArgumentException("Input contains illegal characters")
+    }
+
+    parameters["legacy_subject_id"] = subjectId
     return this
   }
 
-  fun build(): AthenaMonitoringEventsListQuery = AthenaMonitoringEventsListQuery(
-    """
-      SELECT
-          legacy_subject_id
-        , legacy_order_id
-        , event_type
-        , event_date
-        , event_time
-        , event_second
-        , process_date
-        , process_time
-        , process_second
-      FROM 
-        $databaseName.event_history
-      WHERE
-        legacy_subject_id = $legacySubjectId
-    """.trimIndent(),
-    arrayOf(),
-  )
+  fun build(): AthenaMonitoringEventsListQuery = AthenaMonitoringEventsListQuery(getSQL(), parameters.values.toTypedArray())
 }
