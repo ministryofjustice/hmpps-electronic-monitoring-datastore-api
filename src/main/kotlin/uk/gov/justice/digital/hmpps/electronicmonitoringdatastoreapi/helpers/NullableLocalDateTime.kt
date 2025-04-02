f4862567f72d2ab3a3828f79826f8135d4d282e7
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-04-02 11:37:46 +0100

    Correct helper nullableLocalDateTime to account for null time values.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/NullableLocalDateTime.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/helpers/NullableLocalDateTime.kt
@@ -2,4 +2,4 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers
 
 import java.time.LocalDateTime
 
-fun nullableLocalDateTime(date: String?, time: String? = "00:00:00"): LocalDateTime? = if (!date.isNullOrEmpty()) LocalDateTime.parse("${date}T$time") else null
+fun nullableLocalDateTime(date: String?, time: String? = "00:00:00"): LocalDateTime? = if (!date.isNullOrEmpty()) LocalDateTime.parse("${date}T${time ?: "00:00:00"}") else null
