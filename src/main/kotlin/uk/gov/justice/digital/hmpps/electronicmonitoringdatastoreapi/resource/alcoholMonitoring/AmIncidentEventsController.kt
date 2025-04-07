c8314b6058d938d8f47179c4214fd9ff93822f71
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-04-07 15:34:45 +0100

    Create AM events controllers.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/alcoholMonitoring/AmIncidentEventsController.kt
@@ -0,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.resource.alcoholMonitoring
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import jakarta.validation.constraints.Pattern
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.Event
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.alcoholMonitoring.AmIncidentEventDetails
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.AthenaRoleService
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.alcoholMonitoring.AmOrderEventsService
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.internal.AuditService
+
+@RestController
+class AmIncidentEventsController(
+  @Autowired val amOrderEventsService: AmOrderEventsService,
+  val athenaRoleService: AthenaRoleService,
+  @Autowired val auditService: AuditService,
+) {
+
+  @Operation(
+    tags = ["Alcohol monitoring orders"],
+    summary = "Get the incident events for an order",
+  )
+  @RequestMapping(
+    method = [RequestMethod.GET],
+    path = [
+      "/orders/getIncidentEvents/{legacySubjectId}",
+      "/alcohol-monitoring/{legacySubjectId}/incident-events",
+    ],
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+  )
+  @PreAuthorize("hasAnyAuthority('ROLE_EM_DATASTORE_GENERAL_RO', 'ROLE_EM_DATASTORE_RESTRICTED_RO')")
+  fun getIncidentEvents(
+    authentication: Authentication,
+    @Parameter(description = "The legacy subject ID of the order", required = true)
+    @Pattern(
+      regexp = "^[a-zA-Z0-9]+$",
+      message = "Input contains illegal characters - legacy subject ID may only contain letters and numbers",
+    )
+    @PathVariable legacySubjectId: String,
+  ): ResponseEntity<List<Event<AmIncidentEventDetails>>> {
+    val validatedRole = athenaRoleService.getRoleFromAuthentication(authentication)
+
+    val result = amOrderEventsService.getIncidentEvents(legacySubjectId, validatedRole)
+
+    auditService.createEvent(
+      authentication.name,
+      "GET_ALCOHOL_MONITORING_INCIDENT_EVENTS",
+      mapOf(
+        "legacySubjectId" to legacySubjectId,
+        "restrictedOrdersIncluded" to (validatedRole == AthenaRole.ROLE_EM_DATASTORE_RESTRICTED_RO),
+      ),
+    )
+
+    return ResponseEntity.ok(result)
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resources/alcoholMonitoring/AmContactEventsControllerTest.kt
@@ -0,0 +1,80 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.resources.alcoholMonitoring
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import org.springframework.boot.test.autoconfigure.json.JsonTest
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.Authentication
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.Event
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.alcoholMonitoring.AmContactEventDetails
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.resource.alcoholMonitoring.AmContactEventsController
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.AthenaRoleService
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.alcoholMonitoring.AmOrderEventsService
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.internal.AuditService
+import java.time.LocalDateTime
+
+@ActiveProfiles("test")
+@JsonTest
+class AmContactEventsControllerTest {
+  private lateinit var amOrderEventsService: AmOrderEventsService
+  private lateinit var roleService: AthenaRoleService
+  private lateinit var auditService: AuditService
+  private lateinit var controller: AmContactEventsController
+  private lateinit var authentication: Authentication
+
+  @BeforeEach
+  fun setup() {
+    authentication = mock(Authentication::class.java)
+    `when`(authentication.name).thenReturn("MOCK_AUTH_USER")
+    amOrderEventsService = Mockito.mock(AmOrderEventsService::class.java)
+    roleService = mock(AthenaRoleService::class.java)
+    `when`(roleService.getRoleFromAuthentication(authentication)).thenReturn(AthenaRole.ROLE_EM_DATASTORE_GENERAL_RO)
+    auditService = Mockito.mock(AuditService::class.java)
+    controller = AmContactEventsController(amOrderEventsService, roleService, auditService)
+  }
+
+  @Nested
+  inner class GetContactEvents {
+    @Test
+    fun `gets contact events from alcohol monitoring order events service`() {
+      val legacySubjectId = "1ab"
+      val expectedResult = listOf(
+        Event(
+          legacyOrderId = "123",
+          legacySubjectId = "1543",
+          type = "TEST_STATUS",
+          dateTime = LocalDateTime.of(2001, 1, 1, 1, 1, 1),
+          details = AmContactEventDetails(
+            contactDateTime = LocalDateTime.of(2002, 2, 2, 2, 2, 2),
+            inboundOrOutbound = "Inbound",
+            fromTo = "From",
+            channel = "Probation",
+            subjectConsentWithdrawn = "No",
+            callOutcome = "Test call outcome",
+            statement = "Test statement",
+            reasonForContact = "Test contact reason",
+            outcomeOfContact = "Test contact outcome",
+            visitRequired = "No ",
+            visitId = "V001",
+          ),
+        ),
+      )
+
+      `when`(amOrderEventsService.getContactEvents(legacySubjectId, AthenaRole.ROLE_EM_DATASTORE_GENERAL_RO)).thenReturn(expectedResult)
+
+      val result = controller.getContactEvents(authentication, legacySubjectId)
+
+      Assertions.assertThat(result.statusCode).isEqualTo(HttpStatus.OK)
+      Assertions.assertThat(result.body).isEqualTo(expectedResult)
+
+      Mockito.verify(amOrderEventsService, Mockito.times(1)).getContactEvents(legacySubjectId, AthenaRole.ROLE_EM_DATASTORE_GENERAL_RO)
+    }
+  }
+}
