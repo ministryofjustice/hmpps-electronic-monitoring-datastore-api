22b3f89811ed4549d4b0a7115ff18213d1974bd8
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-04-07 15:47:53 +0100

    Create integration tests for AM event controllers.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/integration/resources/alcoholMonitoring/AmContactEventsControllerIntegrationTest.kt
@@ -0,0 +1,67 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.integration.resources.alcoholMonitoring
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.integration.resources.ControllerIntegrationBase
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.mocks.MockEmDatastoreClient
+
+@ActiveProfiles("integration")
+class AmContactEventsControllerIntegrationTest : ControllerIntegrationBase() {
+  @Nested
+  @DisplayName("GET /alcohol-monitoring/{legacySubjectId}/contact-events")
+  inner class GetContactEvents {
+
+    @BeforeEach
+    fun setup() {
+      MockEmDatastoreClient.Companion.addResponseFile("successfulAmContactEventsResponse")
+    }
+
+    @Test
+    fun `should return 401 unauthorized if no authorization header`() {
+      noAuthHeaderRespondsWithUnauthorizedTest("/alcohol-monitoring/234/contact-events")
+    }
+
+    @Test
+    fun `should return 403 forbidden if no role in authorization header`() {
+      noRoleInAuthHeaderRespondsWithForbiddenTest("/alcohol-monitoring/234/contact-events")
+    }
+
+    @Test
+    fun `should return 403 forbidden if wrong role in authorization header`() {
+      wrongRolesRespondsWithForbiddenTest("/alcohol-monitoring/234/contact-events", listOf("ROLE_WRONG"))
+    }
+
+    @Test
+    fun `should throw a Bad Request exception if the URL param format is invalid`() {
+      webTestClient.get()
+        .uri("/alcohol-monitoring/2_4/contact-events")
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus()
+        .isBadRequest
+    }
+
+    @Test
+    fun `should return OK with valid auth header and role for accessing restricted orders`() {
+      webTestClient.get()
+        .uri("/alcohol-monitoring/234/contact-events")
+        .headers(setAuthorisation(roles = listOf("ROLE_EM_DATASTORE_RESTRICTED_RO")))
+        .exchange()
+        .expectStatus()
+        .isOk
+    }
+
+    @Test
+    fun `should return OK with valid auth header and role for accessing general orders`() {
+      webTestClient.get()
+        .uri("/alcohol-monitoring/234/contact-events")
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus()
+        .isOk
+    }
+  }
+}
