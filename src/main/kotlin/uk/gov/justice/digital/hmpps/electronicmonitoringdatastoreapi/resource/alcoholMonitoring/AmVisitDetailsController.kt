a911b14fb4c79c0057ed0424a01ac0474cfce7f1
mattonfoot <matt.smith@justice.gov.uk>
2025-06-26 16:29:13 +0100

    remove athena roles service as not required

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/alcoholMonitoring/AmVisitDetailsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/alcoholMonitoring/AmVisitDetailsController.kt
@@ -24,7 +24,6 @@ import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TAG_ALCOHOL_ORDERS
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TOKEN_HMPPS_AUTH
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.alcoholMonitoring.AmVisitDetails
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.AthenaRoleService
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.alcoholMonitoring.AmVisitDetailsService
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.internal.AuditService
 import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
@@ -33,7 +32,6 @@ import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 @RestController
 class AmVisitDetailsController(
   @param:Autowired private val amVisitDetailsService: AmVisitDetailsService,
-  private val athenaRoleService: AthenaRoleService,
   @param:Autowired private val auditService: AuditService,
 ) {
 
@@ -86,16 +84,14 @@ class AmVisitDetailsController(
     )
     @PathVariable legacySubjectId: String,
   ): ResponseEntity<List<AmVisitDetails>> {
-    val validatedRole = athenaRoleService.getRoleFromAuthentication(authentication)
-
-    val result = amVisitDetailsService.getVisitDetails(legacySubjectId, validatedRole)
+    val result = amVisitDetailsService.getVisitDetails(legacySubjectId, AthenaRole.ROLE_EM_DATASTORE_GENERAL_RO)
 
     auditService.createEvent(
       authentication.name,
       "GET_ALCOHOL_MONITORING_VISIT_DETAILS",
       mapOf(
         "legacySubjectId" to legacySubjectId,
-        "restrictedOrdersIncluded" to (validatedRole == AthenaRole.ROLE_EM_DATASTORE_RESTRICTED_RO),
+        "restrictedOrdersIncluded" to false,
       ),
     )
 
