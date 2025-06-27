a455bc329a86b7d1f3cdf24990c3b2edb45ab791
mattonfoot <matt.smith@justice.gov.uk>
2025-06-27 10:33:27 +0100

    Add restricted query to equipment details

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/service/integrity/IntegrityEquipmentDetailsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/service/integrity/IntegrityEquipmentDetailsService.kt
@@ -1,17 +1,15 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.integrity
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.integrity.IntegrityEquipmentDetails
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.repository.integrity.IntegrityEquipmentDetailsRepository
 
 @Service
 class IntegrityEquipmentDetailsService(
-  @Autowired val integrityEquipmentDetailsRepository: IntegrityEquipmentDetailsRepository,
+  val integrityEquipmentDetailsRepository: IntegrityEquipmentDetailsRepository,
 ) {
-  fun getEquipmentDetails(legacySubjectId: String, role: AthenaRole): List<IntegrityEquipmentDetails> {
-    val result = integrityEquipmentDetailsRepository.getEquipmentDetails(legacySubjectId, role)
+  fun getEquipmentDetails(legacySubjectId: String, restricted: Boolean): List<IntegrityEquipmentDetails> {
+    val result = integrityEquipmentDetailsRepository.getEquipmentDetails(legacySubjectId, restricted)
 
     return result.map { item -> IntegrityEquipmentDetails(item) }
   }
