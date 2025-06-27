75124148546f05971718ca03881f11e7eee3903b
mattonfoot <matt.smith@justice.gov.uk>
2025-06-27 13:05:24 +0100

    Update suspension of visits for restricted access

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/integrity/IntegritySuspensionOfVisitsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/integrity/IntegritySuspensionOfVisitsRepository.kt
@@ -1,9 +1,7 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.repository.integrity
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.EmDatastoreClientInterface
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.AthenaHelper
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.querybuilders.integrity.IntegritySuspensionOfVisitsQueryBuilder
@@ -11,16 +9,16 @@ import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athen
 
 @Service
 class IntegritySuspensionOfVisitsRepository(
-  @Autowired val athenaClient: EmDatastoreClientInterface,
-  @Value("\${services.athena.database}")
+  val athenaClient: EmDatastoreClientInterface,
+  @param:Value("\${services.athena.database}")
   var athenaDatabase: String = "unknown_database",
 ) {
-  fun getSuspensionOfVisits(legacySubjectId: String, role: AthenaRole): List<AthenaIntegritySuspensionOfVisitsDTO> {
+  fun getSuspensionOfVisits(legacySubjectId: String, restricted: Boolean): List<AthenaIntegritySuspensionOfVisitsDTO> {
     val suspensionOfVisitsQuery = IntegritySuspensionOfVisitsQueryBuilder(athenaDatabase)
       .withLegacySubjectId(legacySubjectId)
       .build()
 
-    val athenaResponse = athenaClient.getQueryResult(suspensionOfVisitsQuery, role)
+    val athenaResponse = athenaClient.getQueryResult(suspensionOfVisitsQuery, restricted)
 
     return AthenaHelper.Companion.mapTo<AthenaIntegritySuspensionOfVisitsDTO>(athenaResponse)
   }
