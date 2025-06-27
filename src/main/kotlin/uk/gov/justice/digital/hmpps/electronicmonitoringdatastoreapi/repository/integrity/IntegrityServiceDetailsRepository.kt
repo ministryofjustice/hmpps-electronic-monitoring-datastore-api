ed01a18d7dd9e6485b4260a2efd6680aed1cbea9
mattonfoot <matt.smith@justice.gov.uk>
2025-06-27 11:57:53 +0100

    Add restricted query to service details

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/integrity/IntegrityServiceDetailsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/integrity/IntegrityServiceDetailsRepository.kt
@@ -1,9 +1,7 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.repository.integrity
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.EmDatastoreClientInterface
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.AthenaHelper
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.querybuilders.integrity.IntegrityServiceDetailsQueryBuilder
@@ -11,16 +9,16 @@ import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athen
 
 @Service
 class IntegrityServiceDetailsRepository(
-  @Autowired val athenaClient: EmDatastoreClientInterface,
-  @Value("\${services.athena.database}")
+  val athenaClient: EmDatastoreClientInterface,
+  @param:Value($$"${services.athena.database}")
   var athenaDatabase: String = "unknown_database",
 ) {
-  fun getServiceDetails(legacySubjectId: String, role: AthenaRole): List<AthenaIntegrityServiceDetailsDTO> {
+  fun getServiceDetails(legacySubjectId: String, restricted: Boolean): List<AthenaIntegrityServiceDetailsDTO> {
     val servicesQuery = IntegrityServiceDetailsQueryBuilder(athenaDatabase)
       .withLegacySubjectId(legacySubjectId)
       .build()
 
-    val athenaResponse = athenaClient.getQueryResult(servicesQuery, role)
+    val athenaResponse = athenaClient.getQueryResult(servicesQuery, restricted)
 
     return AthenaHelper.Companion.mapTo<AthenaIntegrityServiceDetailsDTO>(athenaResponse)
   }
