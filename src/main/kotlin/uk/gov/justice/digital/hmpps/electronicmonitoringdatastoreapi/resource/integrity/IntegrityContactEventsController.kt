b35ef348134d2b40cc3d7fdafe6ba424432381cf
mattonfoot <matt.smith@justice.gov.uk>
2025-06-27 10:16:15 +0100

    flag to include restricted orders moved to API querystring

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/integrity/IntegrityContactEventsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/integrity/IntegrityContactEventsController.kt
@@ -17,11 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_GENERAL__RO
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_RESTRICTED__RO
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TAG_INTEGRITY_GENERAL_ORDERS
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TAG_INTEGRITY_RESTRICTED_ORDERS
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TAG_INTEGRITY_ORDERS
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TOKEN_HMPPS_AUTH
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.Event
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.integrity.IntegrityContactEventDetails
@@ -36,8 +34,8 @@ class IntegrityContactEventsController(
 ) {
 
   @Operation(
-    tags = [TAG_INTEGRITY_GENERAL_ORDERS],
-    summary = "Get the contact events for a general integrity order",
+    tags = [TAG_INTEGRITY_ORDERS],
+    summary = "Get the contact events for a integrity order",
   )
   @RequestMapping(
     method = [RequestMethod.GET],
@@ -74,83 +72,24 @@ class IntegrityContactEventsController(
       ),
     ],
   )
-  @SecurityRequirement(name = TOKEN_HMPPS_AUTH, scopes = [ROLE_EM_DATASTORE_GENERAL__RO])
-  @PreAuthorize("hasAnyAuthority('$ROLE_EM_DATASTORE_GENERAL__RO')")
-  fun getGeneralContactEvents(
+  @SecurityRequirement(name = TOKEN_HMPPS_AUTH, scopes = [ROLE_EM_DATASTORE_GENERAL__RO, ROLE_EM_DATASTORE_RESTRICTED__RO])
+  @PreAuthorize("hasAnyAuthority('$ROLE_EM_DATASTORE_GENERAL__RO', '$ROLE_EM_DATASTORE_RESTRICTED__RO')")
+  fun getContactEvents(
     authentication: Authentication,
-    @Parameter(description = "The legacy subject ID of the general integrity order", required = true)
+    @Parameter(description = "The legacy subject ID of the integrity order", required = true)
     @Pattern(regexp = "^[0-9]+$", message = "Input contains illegal characters - legacy subject ID must be a number")
     @PathVariable legacySubjectId: String,
+    @Parameter(description = "A flag to indicate whether to include restricted orders in the resultset")
+    restricted: Boolean = false,
   ): ResponseEntity<List<Event<IntegrityContactEventDetails>>> {
-    val result = integrityOrderEventsService.getContactEvents(legacySubjectId, AthenaRole.ROLE_EM_DATASTORE_GENERAL__RO)
+    val result = integrityOrderEventsService.getContactEvents(legacySubjectId, restricted)
 
     auditService.createEvent(
       authentication.name,
-      "GET_GENERAL_INTEGRITY_CONTACT_EVENTS",
+      "GET_INTEGRITY_CONTACT_EVENTS",
       mapOf(
         "legacySubjectId" to legacySubjectId,
-        "restrictedOrdersIncluded" to false,
-      ),
-    )
-
-    return ResponseEntity.ok(result)
-  }
-
-  @Operation(
-    tags = [TAG_INTEGRITY_RESTRICTED_ORDERS],
-    summary = "Get the contact events for a restricted integrity order",
-  )
-  @RequestMapping(
-    method = [RequestMethod.GET],
-    path = [
-      "/orders/integrity/restricted/{legacySubjectId}/contact-events",
-    ],
-    produces = [MediaType.APPLICATION_JSON_VALUE],
-  )
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "200",
-        description = "OK",
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Bad request - invalid input data.",
-        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
-      ),
-      ApiResponse(
-        responseCode = "401",
-        description = "Unauthorized - requires a valid OAuth2 token",
-        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
-      ),
-      ApiResponse(
-        responseCode = "403",
-        description = "Forbidden - requires an appropriate role",
-        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
-      ),
-      ApiResponse(
-        responseCode = "500",
-        description = "Internal Server Error - An unexpected error occurred.",
-        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
-      ),
-    ],
-  )
-  @SecurityRequirement(name = TOKEN_HMPPS_AUTH, scopes = [ROLE_EM_DATASTORE_RESTRICTED__RO])
-  @PreAuthorize("hasAnyAuthority('$ROLE_EM_DATASTORE_RESTRICTED__RO')")
-  fun getRestrictedContactEvents(
-    authentication: Authentication,
-    @Parameter(description = "The legacy subject ID of the restricted integrity order", required = true)
-    @Pattern(regexp = "^[0-9]+$", message = "Input contains illegal characters - legacy subject ID must be a number")
-    @PathVariable legacySubjectId: String,
-  ): ResponseEntity<List<Event<IntegrityContactEventDetails>>> {
-    val result = integrityOrderEventsService.getContactEvents(legacySubjectId, AthenaRole.ROLE_EM_DATASTORE_RESTRICTED__RO)
-
-    auditService.createEvent(
-      authentication.name,
-      "GET_RESTRICTED_INTEGRITY_CONTACT_EVENTS",
-      mapOf(
-        "legacySubjectId" to legacySubjectId,
-        "restrictedOrdersIncluded" to true,
+        "restrictedOrdersIncluded" to restricted,
       ),
     )
 
--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/integrity/VisitDetailsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/resource/integrity/VisitDetailsController.kt
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_GENERAL__RO
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_RESTRICTED__RO
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TAG_INTEGRITY_GENERAL_ORDERS
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TAG_INTEGRITY_ORDERS
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.TOKEN_HMPPS_AUTH
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.integrity.IntegrityVisitDetails
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.AthenaRoleService
@@ -35,7 +35,7 @@ class VisitDetailsController(
   @field:Autowired val auditService: AuditService,
 ) {
   @Operation(
-    tags = [TAG_INTEGRITY_GENERAL_ORDERS],
+    tags = [TAG_INTEGRITY_ORDERS],
     summary = "Get the visit details for an order",
   )
   @RequestMapping(
