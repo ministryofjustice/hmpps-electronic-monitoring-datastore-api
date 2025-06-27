787c9fe32d69d914728e58887c737e12ca8be6ac
mattonfoot <matt.smith@justice.gov.uk>
2025-06-27 11:57:53 +0100

    Add restricted query to service details

--- src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/integration/resources/integrity/IntegrityServiceDetailsControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/integration/resources/integrity/IntegrityServiceDetailsControllerIntegrationTest.kt
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_GENERAL__RO
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_RESTRICTED__RO
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.integration.resources.ControllerIntegrationBase
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.mocks.MockEmDatastoreClient
 
 @ActiveProfiles("integration")
 class IntegrityServiceDetailsControllerIntegrationTest : ControllerIntegrationBase() {
+
   @Nested
   @DisplayName("GET /orders/integrity/{legacySubjectId}/service-details")
-  inner class GetServiceDetails {
+  inner class GetGeneralServiceDetails {
     @BeforeEach
     fun setup() {
       MockEmDatastoreClient.Companion.addResponseFile("successfulServiceDetailsResponse")
@@ -37,7 +40,7 @@ class IntegrityServiceDetailsControllerIntegrationTest : ControllerIntegrationBa
     fun `should throw a Bad Request exception if the URL param format is invalid`() {
       webTestClient.get()
         .uri("/orders/integrity/2_4/service-details")
-        .headers(setAuthorisation())
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_GENERAL__RO)))
         .exchange()
         .expectStatus()
         .isBadRequest
@@ -47,7 +50,7 @@ class IntegrityServiceDetailsControllerIntegrationTest : ControllerIntegrationBa
     fun `should return OK with valid auth header and role for accessing restricted orders`() {
       webTestClient.get()
         .uri("/orders/integrity/234/service-details")
-        .headers(setAuthorisation(roles = listOf("ROLE_EM_DATASTORE_RESTRICTED_RO")))
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_RESTRICTED__RO)))
         .exchange()
         .expectStatus()
         .isOk
@@ -57,7 +60,56 @@ class IntegrityServiceDetailsControllerIntegrationTest : ControllerIntegrationBa
     fun `should return OK with valid auth header and role for accessing general orders`() {
       webTestClient.get()
         .uri("/orders/integrity/234/service-details")
-        .headers(setAuthorisation())
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_GENERAL__RO)))
+        .exchange()
+        .expectStatus()
+        .isOk
+    }
+  }
+
+  @Nested
+  @DisplayName("GET /orders/integrity/{legacySubjectId}/service-details?restricted=true")
+  inner class GetRestrictedServiceDetails {
+    @BeforeEach
+    fun setup() {
+      MockEmDatastoreClient.Companion.addResponseFile("successfulServiceDetailsResponse")
+    }
+
+    @Test
+    fun `should return 401 unauthorized if no authorization header`() {
+      noAuthHeaderRespondsWithUnauthorizedTest("/orders/integrity/234/service-details?restricted=true")
+    }
+
+    @Test
+    fun `should return 403 forbidden if no role in authorization header`() {
+      noRoleInAuthHeaderRespondsWithForbiddenTest("/orders/integrity/234/service-details?restricted=true")
+    }
+
+    @Test
+    fun `should return 403 forbidden if wrong role in authorization header`() {
+      wrongRolesRespondsWithForbiddenTest("/orders/integrity/234/service-details?restricted=true", listOf("ROLE_WRONG"))
+    }
+
+    @Test
+    fun `should return 403 forbidden if general role in authorization header`() {
+      wrongRolesRespondsWithForbiddenTest("/orders/integrity/234/service-details?restricted=true", listOf(ROLE_EM_DATASTORE_GENERAL__RO))
+    }
+
+    @Test
+    fun `should throw a Bad Request exception if the URL param format is invalid`() {
+      webTestClient.get()
+        .uri("/orders/integrity/2_4/service-details?restricted=true")
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_RESTRICTED__RO)))
+        .exchange()
+        .expectStatus()
+        .isBadRequest
+    }
+
+    @Test
+    fun `should return OK with valid auth header and role for accessing restricted orders`() {
+      webTestClient.get()
+        .uri("/orders/integrity/234/service-details?restricted=true")
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_RESTRICTED__RO)))
         .exchange()
         .expectStatus()
         .isOk
--- src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/integrity/IntegrityServiceDetailsRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/repository/integrity/IntegrityServiceDetailsRepositoryTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.any
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.AthenaRole
+import org.mockito.kotlin.eq
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.client.EmDatastoreClient
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.helpers.AthenaHelper
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.mocks.MockAthenaResultSetBuilder
@@ -103,20 +103,20 @@ class IntegrityServiceDetailsRepositoryTest {
     fun `getServicesList passes correct query to getQueryResult`() {
       val resultSet = AthenaHelper.Companion.resultSetFromJson(serviceDetailsResultSet())
 
-      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
+      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), eq(false))).thenReturn(resultSet)
 
-      repository.getServiceDetails("123", AthenaRole.ROLE_EM_DATASTORE_GENERAL__RO)
+      repository.getServiceDetails("123", false)
 
-      Mockito.verify(emDatastoreClient).getQueryResult(any<AthenaQuery>(), any<AthenaRole>())
+      Mockito.verify(emDatastoreClient).getQueryResult(any<AthenaQuery>(), eq(false))
     }
 
     @Test
     fun `getServicesList returns an AthenaServicesListDTO`() {
       val resultSet = AthenaHelper.Companion.resultSetFromJson(serviceDetailsResultSet())
 
-      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
+      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), eq(false))).thenReturn(resultSet)
 
-      val result = repository.getServiceDetails("123", AthenaRole.ROLE_EM_DATASTORE_GENERAL__RO)
+      val result = repository.getServiceDetails("123", false)
 
       Assertions.assertThat(result).isInstanceOf(List::class.java)
       Assertions.assertThat(result).allSatisfy {
@@ -128,9 +128,9 @@ class IntegrityServiceDetailsRepositoryTest {
     fun `getServicesList returns all the results from getQueryResult`() {
       val resultSet = AthenaHelper.Companion.resultSetFromJson(serviceDetailsResultSet("987"))
 
-      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), any<AthenaRole>())).thenReturn(resultSet)
+      Mockito.`when`(emDatastoreClient.getQueryResult(any<AthenaQuery>(), eq(false))).thenReturn(resultSet)
 
-      val result = repository.getServiceDetails("987", AthenaRole.ROLE_EM_DATASTORE_GENERAL__RO)
+      val result = repository.getServiceDetails("987", false)
 
       Assertions.assertThat(result).isNotNull
       Assertions.assertThat(result.size).isEqualTo(2)
