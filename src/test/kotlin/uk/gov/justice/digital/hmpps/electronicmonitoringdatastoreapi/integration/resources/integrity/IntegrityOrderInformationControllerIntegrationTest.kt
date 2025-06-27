6ad0ef8043f1c35ca5ba6b00621a57f59a754aea
mattonfoot <matt.smith@justice.gov.uk>
2025-06-27 12:09:51 +0100

    Add restricted query to order information

--- src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/integration/resources/integrity/IntegrityOrderInformationControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/integration/resources/integrity/IntegrityOrderInformationControllerIntegrationTest.kt
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_GENERAL__RO
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.config.ROLE_EM_DATASTORE_RESTRICTED__RO
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.integration.resources.ControllerIntegrationBase
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.mocks.MockEmDatastoreClient
 
 @ActiveProfiles("integration")
 class IntegrityOrderInformationControllerIntegrationTest : ControllerIntegrationBase() {
+
   @Nested
   @DisplayName("GET /orders/integrity/{legacySubjectId}")
-  inner class GetSummary {
+  inner class GetGeneralOrderInformation {
     @BeforeEach
     fun setup() {
       MockEmDatastoreClient.Companion.addResponseFile("successfulOrderInformationResponse")
@@ -39,7 +42,7 @@ class IntegrityOrderInformationControllerIntegrationTest : ControllerIntegration
     fun `should throw a Bad Request exception if the URL param format is invalid`() {
       webTestClient.get()
         .uri("/orders/integrity/2_4")
-        .headers(setAuthorisation())
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_GENERAL__RO)))
         .exchange()
         .expectStatus()
         .isBadRequest
@@ -49,7 +52,7 @@ class IntegrityOrderInformationControllerIntegrationTest : ControllerIntegration
     fun `should return OK with valid auth header and role for accessing restricted orders`() {
       webTestClient.get()
         .uri("/orders/integrity/234")
-        .headers(setAuthorisation(roles = listOf("ROLE_EM_DATASTORE_RESTRICTED_RO")))
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_RESTRICTED__RO)))
         .exchange()
         .expectStatus()
         .isOk
@@ -59,7 +62,48 @@ class IntegrityOrderInformationControllerIntegrationTest : ControllerIntegration
     fun `should return OK with valid auth header and role for accessing general orders`() {
       webTestClient.get()
         .uri("/orders/integrity/234")
-        .headers(setAuthorisation())
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_GENERAL__RO)))
+        .exchange()
+        .expectStatus()
+        .isOk
+    }
+  }
+
+  @Nested
+  @DisplayName("GET /orders/integrity/{legacySubjectId}?restricted=true")
+  inner class GetRestrictedOrderInformation {
+    @BeforeEach
+    fun setup() {
+      MockEmDatastoreClient.Companion.addResponseFile("successfulOrderInformationResponse")
+      MockEmDatastoreClient.Companion.addResponseFile("successfulSubjectHistoryReportResponse")
+      MockEmDatastoreClient.Companion.addResponseFile("successfulDocumentListResponse")
+    }
+
+    @Test
+    fun `should return 401 unauthorized if no authorization header`() {
+      noAuthHeaderRespondsWithUnauthorizedTest("/orders/integrity/234?restricted=true")
+    }
+
+    @Test
+    fun `should return 403 forbidden if no role in authorization header`() {
+      noRoleInAuthHeaderRespondsWithForbiddenTest("/orders/integrity/234?restricted=true")
+    }
+
+    @Test
+    fun `should return 403 forbidden if wrong role in authorization header`() {
+      wrongRolesRespondsWithForbiddenTest("/orders/integrity/234?restricted=true", listOf("ROLE_WRONG"))
+    }
+
+    @Test
+    fun `should return 403 forbidden if general role in authorization header`() {
+      wrongRolesRespondsWithForbiddenTest("/orders/integrity/234?restricted=true", listOf(ROLE_EM_DATASTORE_GENERAL__RO))
+    }
+
+    @Test
+    fun `should return OK with valid auth header and role for accessing restricted orders`() {
+      webTestClient.get()
+        .uri("/orders/integrity/234?restricted=true")
+        .headers(setAuthorisation(roles = listOf(ROLE_EM_DATASTORE_RESTRICTED__RO)))
         .exchange()
         .expectStatus()
         .isOk
