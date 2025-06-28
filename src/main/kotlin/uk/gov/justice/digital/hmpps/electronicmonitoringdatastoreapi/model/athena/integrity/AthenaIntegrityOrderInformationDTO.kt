93abd3222b221400deabaa0372e0d0c23f4118e5
mattonfoot <matt.smith@justice.gov.uk>
2025-06-28 15:12:40 +0100

    simplify athena models to remove json helpers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/model/athena/integrity/AthenaIntegrityOrderInformationDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/model/athena/integrity/AthenaIntegrityOrderInformationDTO.kt
@@ -1,22 +1,15 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.integrity
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class AthenaIntegrityOrderInformationDTO(
   val legacySubjectId: String,
   val legacyOrderId: String?,
-  @JsonProperty("full_name")
-  val name: String?,
+  val fullName: String?,
   val alias: String?,
   val dateOfBirth: String?,
-  @JsonProperty("primary_address_line_1")
-  val address1: String?,
-  @JsonProperty("primary_address_line_2")
-  val address2: String?,
-  @JsonProperty("primary_address_line_3")
-  val address3: String?,
-  @JsonProperty("primary_address_post_code")
-  val postcode: String?,
+  val primaryAddressLine1: String?,
+  val primaryAddressLine2: String?,
+  val primaryAddressLine3: String?,
+  val primaryAddressPostCode: String?,
   val orderStartDate: String?,
   val orderEndDate: String?,
 )
