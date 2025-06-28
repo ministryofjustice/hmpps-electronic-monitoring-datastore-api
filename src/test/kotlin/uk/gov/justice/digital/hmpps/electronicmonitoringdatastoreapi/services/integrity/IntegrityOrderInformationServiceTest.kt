dbaa43b103f0442c5f4f675652f1347f0c123cc7
mattonfoot <matt.smith@justice.gov.uk>
2025-06-28 14:54:34 +0100

    Pull out order details and information for Integrity repo

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatastoreapi/services/integrity/IntegrityOrderInformationServiceTest.kt
@@ -0,0 +1,80 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.services.integrity
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.`when`
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.athena.integrity.AthenaIntegrityKeyOrderInformationDTO
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.model.integrity.IntegrityOrderInformation
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.repository.integrity.IntegrityOrderInformationRepository
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatastoreapi.service.integrity.IntegrityOrderInformationService
+import java.time.LocalDateTime
+
+class IntegrityOrderInformationServiceTest {
+  private lateinit var integrityOrderInformationRepository: IntegrityOrderInformationRepository
+  private lateinit var service: IntegrityOrderInformationService
+
+  @BeforeEach
+  fun setup() {
+    integrityOrderInformationRepository = mock(IntegrityOrderInformationRepository::class.java)
+    service = IntegrityOrderInformationService(integrityOrderInformationRepository)
+  }
+
+  @Test
+  fun `AmOrderInformationService can be instantiated`() {
+    Assertions.assertThat(service).isNotNull()
+  }
+
+  @Nested
+  inner class GetOrderInformation {
+    val legacySubjectId = "fake-id"
+
+    val orderInformationData = AthenaIntegrityKeyOrderInformationDTO(
+      legacySubjectId = "123",
+      legacyOrderId = "321",
+      name = "John Smith",
+      alias = "Zeno",
+      dateOfBirth = "1980-02-01",
+      address1 = "1 Primary Street",
+      address2 = "Sutton",
+      address3 = "London",
+      postcode = "ABC 123",
+      orderStartDate = "2012-02-01",
+      orderEndDate = "2013-04-03",
+    )
+
+    @BeforeEach
+    fun setup() {
+      `when`(integrityOrderInformationRepository.getKeyOrderInformation(legacySubjectId, false))
+        .thenReturn(orderInformationData)
+    }
+
+    @Test
+    fun `calls getOrderInformation from order information repository`() {
+      service.getOrderInformation(legacySubjectId, false)
+      Mockito.verify(integrityOrderInformationRepository, times(1)).getKeyOrderInformation(legacySubjectId, false)
+    }
+
+    @Test
+    fun `returns IntegrityOrderInformation`() {
+      val result = service.getOrderInformation(legacySubjectId, false)
+
+      Assertions.assertThat(result).isInstanceOf(IntegrityOrderInformation::class.java)
+    }
+
+    @Test
+    fun `returns correct information of the order`() {
+      val result = service.getOrderInformation(legacySubjectId, false)
+
+      Assertions.assertThat(result).isNotNull
+      Assertions.assertThat(result.keyOrderInformation).isNotNull
+      Assertions.assertThat(result.keyOrderInformation.legacyOrderId).isEqualTo("321")
+      Assertions.assertThat(result.keyOrderInformation.name).isEqualTo("John Smith")
+      Assertions.assertThat(result.keyOrderInformation.dateOfBirth).isEqualTo(LocalDateTime.parse("1980-02-01T00:00:00"))
+    }
+  }
+}
